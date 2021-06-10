a53dec8bb7be2ca84245d67093660a1263e39eb9
largemable <mable.palombo@gmail.com>
2021-06-10 16:10:12 -0500

    add github link

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import APIurl from '../config';
+
 const LogIn = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
